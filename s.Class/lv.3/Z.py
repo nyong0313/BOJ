e066825a1ce0a0978cef57e986df1253bcbf0c22
nyong0313 <ghdyd6625@naver.com>
2025-12-20 23:51:32 +0900

    [Class3] 1074 - bfs, g5 (2025.12.20)

---
+++ s.Class/lv.3/Z.py
@@ -0,0 +1,13 @@
+# 1074
+import sys
+input = sys.stdin.readline
+
+def sol(N,r,c):
+    if N == 0:
+        return 0
+    
+    else:
+        return 2*(r%2)+(c%2) + 4*(sol(N-1, r//2, c//2))
+
+N,r,c = map(int, input().split())
+print(sol(N,r,c))
